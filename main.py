3d5dcd8195761c71eedf41486fde88b6e928e939
painoidntt <166347965+painoidntt@users.noreply.github.com>
2024-06-07 08:49:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ st.title('This is a next level SEO app')
 st.write('Make your idea real')
 st.write('What is your seed keyword?')
 
-st.text_input(label='', value='', labelvisibility='hidden')
+st.text_input(label='', value='', label_visibility='hidden')
